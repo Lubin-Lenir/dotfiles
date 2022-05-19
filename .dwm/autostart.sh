a38327b8bea0fcf6c92253cceee3c5d9dccdb67b
Lubin-Lenir <105852087+Lubin-Lenir@users.noreply.github.com>
2022-05-19 15:21:24 +0530

    Update autostart.sh

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -4,7 +4,7 @@ picom --config ~/.config/picom/picom.conf &
 status () { 
 
 	echo -n " $(pactl list sinks | grep '^[[:space:]]Volume:' | \
-		head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')% |  $(acpi | awk '{print $4}' | sed s/,//) |  $(date '+%d-%m-%Y') |   $(date '+%H:%M')   "
+		head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')% |  $(acpi | awk '{print $4}' | sed s/,//) |  $(date '+%d-%m-%Y') |  $(date '+%H:%M')   "
 }
 
 while :; do
